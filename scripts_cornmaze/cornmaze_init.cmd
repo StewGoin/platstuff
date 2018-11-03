3ee13aa78b153922360c9da6f79a79b3ee3a7971
Jason Ashbaugh <jason.ashbaugh@defpoint.com>
2018-11-03 08:00:08 -0400

    folderizing

---
+++ scripts_cornmaze/cornmaze_init.cmd
@@ -0,0 +1,8 @@
+# .rep "-loot treasure;-stow gem;-get coin;-attack"
+put #var corn_incidentals pass|deed|limb|stack|ear|bead|pile|rattle|bracelet
+put #var corn_grassroom 2
+put #var corn_tokenroom 2
+put #var corn_weedroom 2
+put #var corn_mouseroom 2
+put #var corn_forageroom 2
+put #var corn_traproom 2
