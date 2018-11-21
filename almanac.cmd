3f975c78215295d8d2bdca98d4a2091ff5178fe1
Jason Ashbaugh <jason.ashbaugh@gmail.com>
2018-11-20 20:38:33 -0500

    Add files via upload
    Need to make sure #class almanac_timer is set to True on logon.

---
+++ almanac.cmd
@@ -0,0 +1,2 @@
+#trigger {e/$gametime >= $almanactime/} {#echo >Log Red You can study your almanac.;#class almanac_timer off} {almanac_timer}
+#trigger {^You set about studying your omnibus almanac intently\.} {#var almanactime {#evalmath $gametime+605};#class almanac_timer on}
